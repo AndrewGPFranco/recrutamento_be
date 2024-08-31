8bca28f99dcb3776a2111c8832fdd52c9561eada
AndrewGPSilva <andrewgomes1328@gmail.com>
2024-08-30 23:22:33 -0300

    Criada a entidade Vacancy

---
+++ src/main/java/com/agpf/recrutamento/model/Vacancy.java
@@ -0,0 +1,29 @@
+package com.agpf.recrutamento.model;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity
+public class Vacancy {
+
+    private Long id;
+    private String title;
+    private String description;
+    private BigDecimal salary;
+    private String company;
+    private String location;
+    private List<JobType> jobType;
+    private List<TechnologyType> category;
+    private LevelType levelType;
+    private boolean experience;
+    private StatusType status;
+    private LocalDate created_at;
+    private LocalDate updated_at;
+    private List<String> benefits;
+
+}
