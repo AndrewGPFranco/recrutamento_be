5f6da94fe474f6e01b911aff70366b4b5fd07b16
AndrewGPSilva <andrewgomes1328@gmail.com>
2024-08-30 23:59:05 -0300

    Adicionando anotations na entidade

--- src/main/java/com/agpf/recrutamento/model/Vacancy.java
+++ src/main/java/com/agpf/recrutamento/model/Vacancy.java
@@ -4,8 +4,11 @@ import com.agpf.recrutamento.EnumType.JobType;
 import com.agpf.recrutamento.EnumType.LevelType;
 import com.agpf.recrutamento.EnumType.StatusType;
 import com.agpf.recrutamento.EnumType.TechnologyType;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -15,19 +18,62 @@ import java.util.List;
 @Entity
 public class Vacancy {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Column(name = "title")
+    @Length(min = 5, max = 30)
     private String title;
+
+    @NotNull
+    @Column(name = "description")
+    @Length(min = 10, max = 500)
     private String description;
+
+    @Min(1)
+    @NotNull
+    @Column(name = "salary")
     private BigDecimal salary;
+
+    @NotNull
+    @Column(name = "company")
+    @Length(min = 3, max = 30)
     private String company;
+
+    @NotNull
+    @Column(name = "location")
+    @Length(min = 10, max = 50)
     private String location;
+
+    @NotNull
+    @Column(name = "job")
     private List<JobType> jobType;
-    private List<TechnologyType> category;
+
+    @NotNull
+    @Column(name = "technologies")
+    private List<TechnologyType> technologies;
+
+    @NotNull
+    @Column(name = "level")
     private LevelType levelType;
+
+    @NotNull
+    @Column(name = "experience")
     private boolean experience;
+
+    @Column(name = "status")
     private StatusType status;
+
+    @Column(name = "created_at")
     private LocalDate created_at;
+
+    @Column(name = "updated_at")
     private LocalDate updated_at;
+
+    @NotNull
+    @Column(name = "benefits")
     private List<String> benefits;
 
 }
