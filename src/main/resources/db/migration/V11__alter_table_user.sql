6140f2443272fa6a6d3faab80e4cb39f1de5d21e
AndrewGPFranco <andrewgomes1328@gmail.com>
2024-10-10 22:47:52 -0300

    fix: ajustando sql quebrado quando sobe o sistema do 0

--- src/main/resources/db/migration/V11__alter_table_user.sql
+++ src/main/resources/db/migration/V11__alter_table_user.sql
@@ -1,13 +1,10 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1
-        FROM information_schema.columns
-        WHERE table_name = 'users'
-          AND column_name = 'profile_id'
-    ) THEN
-        ALTER TABLE users
-        ADD COLUMN profile_id BIGINT,
-        ADD CONSTRAINT fk_profile FOREIGN KEY (profile_id) REFERENCES profiles(id) ON DELETE CASCADE;
-    END IF;
-END $$;
+SELECT COUNT(*)
+INTO @colExists
+FROM information_schema.columns
+WHERE table_name = 'users'
+AND column_name = 'profile_id';
+
+SET @alterStmt = IF(@colExists = 0, 'ALTER TABLE users ADD COLUMN profile_id BIGINT, ADD CONSTRAINT fk_profile FOREIGN KEY (profile_id) REFERENCES profiles(id) ON DELETE CASCADE', 'SELECT "Column already exists"');
+PREPARE stmt FROM @alterStmt;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
